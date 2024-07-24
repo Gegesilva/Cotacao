6ee4ee51ec7d6e862f5174b12fc39d1caa78b19c
Gegesilva <103137242+Gegesilva@users.noreply.github.com>
2024-07-24 16:21:55 -0300

    novo

---
+++ DB/dados.php
@@ -0,0 +1,31 @@
+<?php
+function preenchimento($conn, $serie)
+{
+    $sql = "SELECT 
+                TB02112_ESTADO Estado,
+                TB01008_NOME Cliente,
+                TB02112_LOCAL Local,
+                TB02112_EMAIL Email,
+                9999 UltCont,
+                TB02112_NUMSERIE Serie
+                
+            FROM TB02112
+            LEFT JOIN TB02111 ON TB02111_CODIGO = TB02112_CODIGO
+            LEFT JOIN TB01008 ON TB01008_CODIGO = TB02111_CODCLI
+
+            WHERE TB02112_NUMSERIE = '$serie'
+    ";
+    $stmt = sqlsrv_query($conn, $sql);
+
+   while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        $estado = $row['Estado'];
+        $Cliente = $row['Cliente'];
+        $Local = $row['Local'];
+        $UltCont = $row['UltCont'];
+        $Email = $row['Email'];
+        $Serie= $row['Serie'];
+    }
+
+    return [$estado, $Cliente, $Local, $UltCont, $Email, $Serie];
+
+}
---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+header('Content-type: text/html; charset=ISO-8895-1');
+include_once "DB/conexaoSQL.php";
+include_once "DB/dados.php";
+
+$serie = $_GET["serie"];
+
+list($estado, $Cliente, $Local, $UltCont, $Email, $Serie) = preenchimento($conn, $serie);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATABIT</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+
+<body>
+    <form>
+        <img src="img/logo.jpg" alt="logo">
+        <h1>ABERTURA CHAMADO TEC</h1>
+        <div class="form-group">
+            <div class="form-input">
+                <label for="patrimonio">Estado *</label>
+                <input type="text" id="patrimonio" name="patrimonio" value="<?= $estado ?>" required>
+            </div>
+            <div class="form-input">
+                <label for="serie">Série *</label>
+                <input id="serie" name="serie" rows="4" placeholder="<?= $serie;?>" required></input>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="form-input">
+                <label for="cliente">Nome do Cliente</label>
+                <input type="text" id="cliente" name="cliente" placeholder="<?= $Cliente; ?>" value="<?= $Cliente; ?>">
+            </div>
+            <div class="form-input">
+                <label for="local">Local*</label>
+                <input type="text" id="local" name="local" placeholder="<?= $Local; ?>" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="form-input">
+                <label for="abertura">Solicitante *</label>
+                <input type="text" id="abertura" name="abertura" required>
+            </div>
+            <div class="form-input">
+                <label for="email">E-mail</label>
+                <input type="email" id="email" name="email" placeholder="<?= $Email; ?>" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="form-input">
+                <label for="whatsapp">Whatsapp *</label>
+                <input type="text" id="whatsapp" name="whatsapp" placeholder="<?= $Tel?>" required>
+            </div>
+        </div>
+        <div class="form-group">
+        <div class="form-input">
+                <label for="defeito">Defeito Apresentado *</label>
+                <textarea id="defeito" name="defeito" rows="4" required></textarea>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="form-input">
+                <label for="contador">Último Contador</label>
+                <input type="text" id="contador" name="contador" placeholder="<?= $UltCont; ?>">
+            </div>
+            <div class="form-input">
+                <label for="periodo">Período</label>
+                <select id="periodo" name="periodo" required>
+                    <option value="Manhã">Manhã</option>
+                    <option value="Tarde">Tarde</option>
+                    <option value="Indiferente">Indiferente</option>
+                </select>
+            </div>
+        </div>
+        <button type="submit" class="submit-btn">Enviar OS</button>
+    </form>
+</body>
+
+</html>
