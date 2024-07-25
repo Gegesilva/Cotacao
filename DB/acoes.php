eb5989d6ab9316d85e0bc8a848962d9415d03616
Gegesilva <103137242+Gegesilva@users.noreply.github.com>
2024-07-25 14:34:59 -0300

    25-07 14:34

--- DB/acoes.php
+++ DB/acoes.php
@@ -1,17 +1,6 @@
 <?php
-/* header('Content-type: text/html; charset=ISO-8895-1');
-
-$estado = $_POST['estado'];
-$local = $_POST['local'];
-$email = $_POST['email'];
-$contpb = $_POST['contador'];
-$serie = $_POST['serie'];
-$whatsapp = $_POST['whatsapp']; */
-
-
 function gravaOS($conn, $estado, $local, $email, $contpb, $serie, $whatsapp, $solicitante, $defeito)
 {
-    /* global $estado, $local, $email, $contpb, $serie, $whatsapp; */
 
     $sql = "INSERT INTO TB02115 ( 
         TB02115_CODIGO,
@@ -25,7 +14,7 @@ function gravaOS($conn, $estado, $local, $email, $contpb, $serie, $whatsapp, $so
 		TB02115_SOLICITANTE,
 		TB02115_OBS)
         VALUES(
-           (SELECT TOP 1 TB02115_CODIGO + 1 FROM TB02115 ORDER BY TB02115_DTCAD DESC),
+            (SELECT TOP 1 TB02115_CODIGO + 1 FROM TB02115 ORDER BY TB02115_DTCAD DESC),
            GETDATE(),
            $estado,
            $local, 
