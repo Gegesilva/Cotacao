3bcb0573e6a6f1b6bb96c7706565b5eff33e230a
Gegesilva <103137242+Gegesilva@users.noreply.github.com>
2024-07-30 17:02:18 -0300

    30-07 17:02

--- DB/dados.php
+++ DB/dados.php
@@ -1,6 +1,22 @@
 <?php
 function preenchimento($conn, $serie)
 {
+    $sql = "SELECT TOP 1 1 existPat FROM TB02055
+        WHERE TB02055_PAT = '$serie'
+    ";
+    $stmt = sqlsrv_query($conn, $sql);
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        $existPat .= $row['existPat'];
+    }
+
+    if($existPat == '1'){
+        $filtroPatSerie = "TB02112_PAT = '$serie'";
+    }else{
+        $filtroPatSerie = "TB02112_NUMSERIE = '$serie'";
+    }
+
+
+
     $sql = "SELECT 
                 TB02112_ESTADO Estado,
                 TB01008_NOME Cliente,
@@ -14,7 +30,7 @@ function preenchimento($conn, $serie)
             LEFT JOIN TB02111 ON TB02111_CODIGO = TB02112_CODIGO
             LEFT JOIN TB01008 ON TB01008_CODIGO = TB02111_CODCLI
 
-            WHERE TB02112_NUMSERIE = '$serie'
+            WHERE $filtroPatSerie
     ";
     $stmt = sqlsrv_query($conn, $sql);
 
@@ -37,10 +53,13 @@ function PegaTipo($conn, $serie)
     $sql = "SELECT TOP 1
                 CASE 
                     WHEN TB02115_PREVENTIVA = 'N' THEN 'NORMAL'
-                    WHEN TB02115_PREVENTIVA = 'P' THEN 'PREVENTIVA'
+                    WHEN TB02115_PREVENTIVA = 'S' THEN 'PREVENTIVA'
                     WHEN TB02115_PREVENTIVA = 'I' THEN 'INSTALAÇÃO'
                     WHEN TB02115_PREVENTIVA = 'D' THEN 'DESINSTALAÇÃO'
+                    WHEN TB02115_PREVENTIVA = 'R' THEN 'RETORNO/RECARGA'
                     WHEN TB02115_PREVENTIVA = 'A' THEN 'AFERIÇÃO'
+                    WHEN TB02115_PREVENTIVA = 'B' THEN 'ATEND. BALCÃO'
+                    WHEN TB02115_PREVENTIVA = 'E' THEN 'ESTOQUE'
                 END Tipo
             FROM TB02115 
             WHERE TB02115_NUMSERIE = '$serie' 
